0857e0e6cfdc6e288caa528e38b36da8fc66cdf9
borbach <54514786+borbach@users.noreply.github.com>
2023-12-03 17:40:40 -0500

    Add files via upload

---
+++ draw_all.cpp
@@ -0,0 +1,44 @@
+#include <ncurses.h>
+
+void drawCat(int y, int x) {
+    mvprintw(y, x, " /\\_/\\ ");
+    mvprintw(y + 1, x, "( o.o )");
+    mvprintw(y + 2, x, " > ^ < ");
+}
+
+void drawDog(int y, int x) {
+    mvprintw(y, x, " / \\__");
+    mvprintw(y + 1, x, "(    @\\___");
+    mvprintw(y + 2, x, " /         O");
+    mvprintw(y + 3, x, "/   (_____ /");
+}
+
+void drawPerson(int y, int x) {
+    mvprintw(y, x, " O");
+    mvprintw(y + 1, x, "/|\\");
+    mvprintw(y + 2, x, "/ \\");
+}
+
+int main() {
+    // Initialize ncurses
+    initscr();
+    // Don't display cursor
+    curs_set(0);
+
+    // Draw the cat, dog, and person
+    drawCat(1, 5);
+    drawDog(7, 5);
+    drawPerson(13, 5);
+
+    // Refresh the screen
+    refresh();
+
+    // Wait for user input
+    getch();
+
+    // Clean up and exit
+    endwin();
+
+    return 0;
+}
+
---
+++ draw_cat.cpp
@@ -0,0 +1,34 @@
+#include <ncurses.h>
+
+int main() {
+    // Initialize ncurses
+    initscr();
+    // Don't display cursor
+    curs_set(0);
+
+    // Draw the cat
+    mvprintw(1, 5, " /\\_/\\ ");
+    mvprintw(2, 5, "( o.o )");
+    mvprintw(3, 5, " > ^ < ");
+    mvprintw(5, 5, " /\\_/\\ ");
+    mvprintw(6, 5, "( o.o )");
+    mvprintw(7, 5, " > ^ < ");
+    mvprintw(9, 5, " /\\_/\\ ");
+    mvprintw(10, 5, "( o.o )");
+    mvprintw(11, 5, " > ^ < ");
+    mvprintw(13, 5, " /\\_/\\ ");
+    mvprintw(14, 5, "( o.o )");
+    mvprintw(15, 5, " > ^ < ");
+
+    // Refresh the screen
+    refresh();
+    
+    // Wait for user input
+    getch();
+
+    // Clean up and exit
+    endwin();
+
+    return 0;
+}
+
