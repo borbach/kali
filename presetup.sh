961adaedb196eefe7a5ae4edc2a948342ad8f8a8
borbach <54514786+borbach@users.noreply.github.com>
2023-12-06 14:10:40 -0500

    Add files via upload

--- presetup.sh
+++ presetup.sh
@@ -10,6 +10,16 @@ lspci -k | grep -A 3 Audio
 aplay -L
 pulseaudio
 pavucontrol
+efibootmgr
+#In windows bcdedit /enum FIRMWARE
 
+sudo grub-mkconfig > /boot/grub/grub.cfg
 
+efiboptmgr
+#There are various options with the efibootmgr command
+
+sudo apt install grub-efi
+#This will modift the /boot/efi/EFI/BOOT/BOOTX64.EFI
+#As weil as the /boot/efi/EFI/kali/grubx64.efi
+#
 
