ffa116c72ca0dc374f6fd9a464723f184f582a9c
borbach <54514786+borbach@users.noreply.github.com>
2023-12-11 10:47:23 -0500

    Add files via upload

--- ch.sh
+++ ch.sh
@@ -5,19 +5,35 @@ mkdir -p $chr/lib
 mkdir -p $chr/lib64
 cd $chr
 cp -v /bin/bash $chr/bin
-cp -v /bin/touch $chr/bin
-cp -v /bin/ls $chr/bin
-cp -v /bin/rm $chr/bin
-cp -v /bin/find $chr/bin
 list="$(ldd /bin/bash | egrep -o '/lib.*\.[0-9]')"
 echo $list
 for i in $list; do cp -v --parents "$i" "${chr}"; done
+cp -v /bin/touch $chr/bin
 list="$(ldd /bin/touch | egrep -o '/lib.*\.[0-9]')"
+echo $list
 for i in $list; do cp -v --parents "$i" "${chr}"; done
+cp -v /bin/ls $chr/bin
 list="$(ldd /bin/ls | egrep -o '/lib.*\.[0-9]')"
+echo $list
 for i in $list; do cp -v --parents "$i" "${chr}"; done
-list="$(ldd /bin/ls | egrep -o '/lib.*\.[0-9]')"
+cp -v /bin/rm $chr/bin
+list="$(ldd /bin/rm | egrep -o '/lib.*\.[0-9]')"
+echo $list
 for i in $list; do cp -v --parents "$i" "${chr}"; done
+cp -v /bin/find $chr/bin
 list="$(ldd /bin/find | egrep -o '/lib.*\.[0-9]')"
+echo $list
+for i in $list; do cp -v --parents "$i" "${chr}"; done
+cp -v /usr/sbin/grub-install $chr/bin
+list="$(ldd /usr/sbin/grub-install | egrep -o '/lib.*\.[0-9]')"
+echo $list
+for i in $list; do cp -v --parents "$i" "${chr}"; done
+cp -v /usr/sbin/update-grub $chr/bin
+list="$(ldd /usr/sbin/update-grub | egrep -o '/lib.*\.[0-9]')"
+echo $list
+for i in $list; do cp -v --parents "$i" "${chr}"; done
+cp -v /usr/sbin/grub-mkconfig $chr/bin
+list="$(ldd /usr/sbin/grub-mkconfig | egrep -o '/lib.*\.[0-9]')"
+echo $list
 for i in $list; do cp -v --parents "$i" "${chr}"; done
 sudo chroot $chr /bin/bash
