55efd4505f15725f3bcc2dc56752cdce78b5081e
borbach <54514786+borbach@users.noreply.github.com>
2023-12-21 15:52:59 -0500

    Add files via upload

--- ch.sh
+++ ch.sh
@@ -1,66 +1,30 @@
-chr=/home/bill/testroot
-mkdir -p $chr
-mkdir -p $chr/bin
-mkdir -p $chr/lib
-mkdir -p $chr/lib64
-cd $chr
-cp -v /bin/bash $chr/bin
-list="$(ldd /bin/bash | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/touch $chr/bin
-list="$(ldd /bin/touch | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/ls $chr/bin
-list="$(ldd /bin/ls | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/rm $chr/bin
-list="$(ldd /bin/rm | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/find $chr/bin
-list="$(ldd /bin/find | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/grep $chr/bin
-list="$(ldd /bin/grep | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /usr/sbin/grub-install $chr/bin
-list="$(ldd /usr/sbin/grub-install | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /usr/sbin/update-grub $chr/bin
-list="$(ldd /usr/sbin/update-grub | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /usr/sbin/grub-mkconfig $chr/bin
-list="$(ldd /usr/sbin/grub-mkconfig | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/df $chr/bin
-list="$(ldd /bin/df | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -v /bin/lsblk $chr/bin
-list="$(ldd /bin/lsblk | egrep -o '/lib.*\.[0-9]')"
-echo $list
-for i in $list; do cp -v --parents "$i" "${chr}"; done
-cp -r -v /boot $chr
-mkdir $chr/usr
-mkdir $chr/usr/lib
-mkdir $chr/usr/share
-cp -r -v /usr/lib/grub $chr/usr/lib/grub/
-cp -r -v /usr/share/grub $chr/usr/share/grub/
-#mkdir $chr/etc
-cp -r -v /dev/ $chr/dev/
-cp -r -v /etc/ $chr/etc/
-sudo chmod 777 /etc/grub.d/*
+mount $1 /mnt/sd
+cd /mnt/sd
+mkdir dev
+mkdir bin
+mkdir lib
+mkdir etc 
+mkdir lib32 
+mkdir lib64
+mkdir libx32 
+mkdir usr 
+mkdir usr/lib 
+mkdir usr/lib/x86_64-linux-gnu 
+mkdir usr/lib/x86_64-linux-gnu/zsh
+mkdir usr/lib/x86_64-linux-gnu/zsh/5.9
+mkdir usr/lib/x86_64-linux-gnu/zsh/5.9/zsh
+mkdir usr/bin
+mkdir usr/bin/zsh
+mkdir usr/bin/zsh5
+mount --bind /dev /mnt/sd/dev
+mount --bind /bin /mnt/sd/bin
+mount --bind /lib /mnt/sd/lib
+mount --bind /etc /mnt/sd/etc
+mount --bind /lib32 /mnt/sd/lib32
+mount --bind /lib64 /mnt/sd/lib64
+mount --bind /libx32 /mnt/sd/libx32
+mount --bind /usr/lib/x86_64-linux-gnu/zsh/5.9/zsh /mnt/sd/usr/lib/x86_64-linux-gnu/zsh/5.9/zsh
+mount --bind /usr/bin/zsh /mnt/sd/usr/bin/zsh
+mount --bind /usr/bin/zsh /mnt/sd/usr/bin/zsh5
+chroot /mnt/sd
 
-mkdir $chr/sys
-mkdir $chr/sys/firmware
-cp -r -v /sys/firmware/efi $chr/sys/firmware/efi/
-
-sudo chroot $chr /bin/bash
