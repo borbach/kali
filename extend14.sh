2b81bf43f8ac598e1684fc5b8577b9d7817a2df4
borbach <54514786+borbach@users.noreply.github.com>
2024-06-04 21:08:26 -0400

    Add files via upload

---
+++ extend14.sh
@@ -0,0 +1,30 @@
+fdisk -l
+sleep 10
+fdisk /dev/mmcblk1
+sleep 10
+fdisk -l
+sleep 10
+pvdisplay
+sleep 10
+pvcreate /dev/mmcblk1p6
+sleep 10
+pvdisplay
+sleep 10
+vgdisplay | grep Name
+sleep 10
+vgextend asus14-vg /dev/mmcblk1p6
+sleep 10
+vgdisplay
+sleep 10
+lvdisplay | grep Path
+sleep 10
+lvextend -l +100%FREE /dev/asus14-vg/root
+sleep 10
+lvdisplay
+sleep 10
+resize2fs /dev/asus14-vg/root
+sleep 10
+df -lh
+sleep 10
+
+
