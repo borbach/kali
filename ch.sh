d931f455837dad36d23286b7a63c48187596c9c0
borbach <54514786+borbach@users.noreply.github.com>
2024-07-16 11:37:23 -0400

    Add files via upload

--- ch.sh
+++ ch.sh
@@ -1,30 +1,42 @@
-mount $1 /mnt/sd
-cd /mnt/sd
+
+cd /mnt
+mkdir proc
+mkdir sys
 mkdir dev
+mkdir sbin
 mkdir bin
-mkdir lib
-mkdir etc 
-mkdir lib32 
-mkdir lib64
-mkdir libx32 
 mkdir usr 
-mkdir usr/lib 
-mkdir usr/lib/x86_64-linux-gnu 
-mkdir usr/lib/x86_64-linux-gnu/zsh
-mkdir usr/lib/x86_64-linux-gnu/zsh/5.9
-mkdir usr/lib/x86_64-linux-gnu/zsh/5.9/zsh
-mkdir usr/bin
-mkdir usr/bin/zsh
-mkdir usr/bin/zsh5
-mount --bind /dev /mnt/sd/dev
-mount --bind /bin /mnt/sd/bin
-mount --bind /lib /mnt/sd/lib
-mount --bind /etc /mnt/sd/etc
-mount --bind /lib32 /mnt/sd/lib32
-mount --bind /lib64 /mnt/sd/lib64
-mount --bind /libx32 /mnt/sd/libx32
-mount --bind /usr/lib/x86_64-linux-gnu/zsh/5.9/zsh /mnt/sd/usr/lib/x86_64-linux-gnu/zsh/5.9/zsh
-mount --bind /usr/bin/zsh /mnt/sd/usr/bin/zsh
-mount --bind /usr/bin/zsh /mnt/sd/usr/bin/zsh5
-chroot /mnt/sd
+mkdir lib 
+mkdir lib32 
+mkdir lib64 
+mkdir boot 
+mkdir etc 
+mkdir home 
+mkdir media 
+mkdir opt 
+mkdir root 
+mkdir srv 
+mkdir sys
+mount --bind /proc /mnt/proc
+mount --bind /sys /mnt/sys
+mount --bind /dev /mnt/dev
+mount --bind /sbin /mnt/sbin
+mount --bind /bin /mnt/bin
+mount --bind /usr /mnt/usr
+mount --bind /lib /mnt/lib
+mount --bind /lib32 /mnt/lib32
+mount --bind /lib64 /mnt/lib64
+mount --bind /lib64 /mnt/lib64
+mount --bind /boot /mnt/boot/
+mount --bind /etc /mnt/etc
+mount --bind /home /mnt/home
+mount --bind /media /mnt/media
+mount --bind /opt /mnt/opt
+mount --bind /root /mnt/root
+mount --bind /srv /mnt/srv
+mount --bind /sys /mnt/sys
+mount --bind /lib64 /mnt/lib64
+mount --bind /lib64 /mnt/lib64
+
+chroot /mnt /bin/zsh
 
