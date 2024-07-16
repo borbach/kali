0d92c3aea863f69cb5891bba75c15dba42b5c487
borbach <54514786+borbach@users.noreply.github.com>
2024-07-16 14:05:05 -0400

    Add files via upload

---
+++ unch.sh
@@ -0,0 +1,6 @@
+umount /mnt/proc /mnt/sys /mnt/dev
+umount /mnt/sbin /mnt/bin /mnt/usr
+umount /mnt/lib /mnt/lib32 /mnt/lib64
+umount /mnt/boot /mnt/etc /mnt/home
+umount /mnt/media /mnt/opt /mnt/root
+umount /mnt/srv /mnt/sys 
